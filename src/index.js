3a17bf0164b089fdf43691743b8c20f8a601ab25
Atharv1903 <awishkar193@gmail.com>
2024-02-21 18:40:53 +0530

    modification

--- src/index.js
+++ src/index.js
@@ -6,14 +6,3 @@ dotenv.config({
 })
 
 connectDB()
-
-/*
-( async () => {
-    try{
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-    }catch(error){
-        console.error("ERROR: ",error);
-        throw err;
-    }
-})()
-*/
